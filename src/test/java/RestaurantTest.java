dc7270e421896e171ddbd416bf70bd27ca0e24e6
sadiq <sadiqhudli55@outlook.com>
2023-09-19 01:57:01 +0530

    Failing_Test_case

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -2,6 +2,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -88,13 +90,13 @@ class RestaurantTest {
 
 
 
-    /*
-    // 2 comimt
+
+
     @Test
     public void TotalAmountOf_Selected_Menu_items(){
         LocalTime openingTime = LocalTime.parse("10:30:00");
         LocalTime closingTime = LocalTime.parse("22:00:00");
-        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant =new Restaurant("TajMIni","belgaum",openingTime,closingTime);
         restaurant.addToMenu("Sweet corn soup",119);
         restaurant.addToMenu("Vegetable lasagne", 269);
         restaurant.addToMenu("Patsa Alfredo",120);
@@ -109,5 +111,5 @@ class RestaurantTest {
     }
 
 
-     */
+
 }
